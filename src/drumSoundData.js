5bf8b8cac2d019fed5ea9eccaa62fea2f5b73754
JongWoo Ha <jongwooha98@gmail.com>
2021-06-24 00:30:46 +0900

    create json including drum sounds

---
+++ src/drumSoundData.js
@@ -0,0 +1,48 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 'Heater 1',
+    value: 'Q',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+  },
+  {
+    id: 'Heater 2',
+    value: 'W',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+  },
+  {
+    id: 'Heater 3',
+    value: 'E',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+  },
+  {
+    id: 'Kick n Hat',
+    value: 'A',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+  },
+  {
+    id: 'RP4 KICK 1',
+    value: 'S',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+  },
+  {
+    id: 'Punchy Kick 1',
+    value: 'D',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  },
+  {
+    id: 'Side Stick 1',
+    value: 'Z',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  },
+  {
+    id: 'Brk Snare',
+    value: 'X',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  },
+  {
+    id: 'Bld H1',
+    value: 'C',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  },
+];
