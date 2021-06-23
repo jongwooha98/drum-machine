ffa832c229784d9e5f082bdc4a0d55f57202d2f6
JongWoo Ha <jongwooha98@gmail.com>
2021-06-24 00:32:06 +0900

    add drumpad color change when key pressed

--- src/App.js
+++ src/App.js
@@ -1,68 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
-
-const data = [
-  {
-    id: 'Heater 1',
-    value: 'Q',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
-  },
-  {
-    id: 'Heater 2',
-    value: 'W',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
-  },
-  {
-    id: 'Heater 3',
-    value: 'E',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
-  },
-  {
-    id: 'Kick n Hat',
-    value: 'A',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
-  },
-  {
-    id: 'RP4 KICK 1',
-    value: 'S',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
-  },
-  {
-    id: 'Punchy Kick 1',
-    value: 'D',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
-  },
-  {
-    id: 'Side Stick 1',
-    value: 'Z',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
-  },
-  {
-    id: 'Brk Snare',
-    value: 'X',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
-  },
-  {
-    id: 'Bld H1',
-    value: 'C',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
-  },
-];
+import './App.scss';
+import drumSoundData from './drumSoundData';
 
 class DrumPad extends React.Component {
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener('keyup', this.handleKeyUp);
   }
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyDown);
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
   handleKeyDown = (e) => {
     console.log(e.key);
     if (e.keyCode === this.props.value.charCodeAt()) {
+      document.getElementById(`${this.props.value}`).classList.add('active');
       this.handleClick();
     }
+  };
+
+  handleKeyUp = (e) => {
+    document.getElementById(`${this.props.value}`).classList.remove('active');
   };
 
   handleClick = () => {
@@ -75,9 +35,10 @@ class DrumPad extends React.Component {
   render() {
     return (
       <div id={this.props.id} className="drum-pad" onClick={this.handleClick}>
-        <button>{this.props.value}</button>
+        <button id={this.props.value} className="drum-pad__button">
+          {this.props.value}
+        </button>
         <audio
-          id={this.props.value}
           className="clip"
           src={this.props.src}
           ref={(ref) => (this.audio = ref)}
@@ -106,11 +67,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <div id="drum-machine">
-        <div id="display">{this.state.display}</div>
-        <div>
-          {data.map((d) => (
+      <div className="drum-machine" id="drum-machine">
+        <h1>Drum Machine</h1>
+        <div className="display" id="display">
+          {this.state.display}
+        </div>
+        <div className="drum-pad__container">
+          {drumSoundData.map((d, index) => (
             <DrumPad
+              key={index}
               id={d.id}
               value={d.value}
               src={d.src}
